cc3be04f7a8b130d6a77e6eb1fecd6d8d3601ecd
Corey Oordt <coreyoordt@gmail.com>
2020-12-27 08:51:07 -0600

    WIP

--- adr/cli.py
+++ adr/cli.py
@@ -1,123 +1,47 @@
 import click
-import click_logger
+import click_config_file
 from environs import Env
 
 env = Env()
 env.read_env()  # read .env file, if it exists
+provider = click_config_file.configobj_provider(section="adr", unrepr=False)
 
 
-@click.command()
-@click_logger.log_level()
-@click.option(
-    "--input-queue",
-    "-i",
-    required=True,
-    type=str,
-    envvar="ADR_QUEUE",
-    help="The name of the Redis input queue",
-)
-@click.option(
-    "--output-queue",
-    "-o",
-    required=True,
-    type=str,
-    envvar="ADR_OUTPUT_QUEUE",
-    help="The name of the Redis output queue",
-)
-@click.option(
-    "--audit-queue",
-    required=True,
-    default="AUDIT",
-    type=str,
-    envvar="AUDIT_QUEUE",
-    help="The name of the Redis queue for the audit logs",
-)
-@click.option(
-    "--redis-url",
-    "-r",
-    default="redis://localhost/0",
-    type=str,
-    show_default=True,
-    envvar="REDIS_URL",
-    help="The address of the Redis server",
-)
-@click.option(
-    "--environment",
-    default="DEV",
-    type=str,
-    show_default=True,
-    envvar="ENVIRONMENT",
-    help="Environment we're running in (DEV / PROD / ...)",
-)
-def cli(
-    log_level: str,
-    input_queue: str,
-    output_queue: str,
-    audit_queue: str,
-    redis_url: str,
-    environment: str
-):
+@click.group()
+def cli():
     """
-    Main entrypoint for execution
-
-    Args:
-        log_level:
-        input_queue:
-        output_queue:
-        audit_queue:
-        redis_url:
-        environment:
+    Manage Architectural Decision Records
     """
-    import adr
-    from .adr_main import main
-    from click_logger.apm import create_apm_client
-    from streams import Stream
-    from streams.types import QueueConfig
-    import redis
+    pass
 
-    is_debug = environment.lower() not in ("prod", "dev", "notprod")
-    label_map = {"kafka_uid": "load.kafka_event_id", "load_number": "load.load_num"}
-    apm_client = create_apm_client(
-        "adr",
-        adr.__version__,
-        environment,
-        is_debug,
-        label_map,
-    )
 
-    redis_client = redis.Redis.from_url(redis_url)
-    redis_options = redis_client.connection_pool.connection_kwargs.copy()
-    redis_host = redis_options.pop("host")  # NOQA
-    redis_port = redis_options.pop("port")  # NOQA
-    if redis_url.startswith("rediss://"):
-        redis_options["ssl"] = True
+@cli.command()
+@click.option(
+    "--directory",
+    "-d",
+    default="./doc/adr",
+    type=click.Path(file_okay=False, dir_okay=True, writable=True, resolve_path=True),
+)
+@click.option("--template", "-t", type=click.Path(dir_okay=False, resolve_path=True))
+@click.pass_context
+@click_config_file.configuration_option(
+    provider=provider, cmd_name="adr", config_file_name=".adr.cfg"
+)
+def init(ctx, directory: str, template: str):
+    """
+    Initializes the directory of architecture decision records:
 
-    click_logger.config(
-        log_level=log_level,
-        client=redis_client,
-        version=adr.__version__,
-        app_name="adr",
-        queue_name=audit_queue,
-        extract_keys=None,  # TODO can we really not add anything to extract_keys?
-    )
+        * creates a subdirectory of the current working directory
 
-    input_q = QueueConfig(
-        queue_name=input_queue,
-        client=redis_client,
-    )
-    output_q = QueueConfig(
-        queue_name=output_queue,
-        client=redis_client,
-    )
-    stream = Stream(
-        apm_client=apm_client,
-        input_queue=input_q,
-        output_queue=output_q,
-        processing_func=main,
-        fail_silently=False,
-    )
+        * creates the first ADR in that subdirectory, recording the decision to record architectural decisions with ADRs.
 
-    stream.queue_func_queue()
+    If the DIRECTORY is not given, the ADRs are stored in the directory `doc/adr`.
+    """
+    # from pathlib import Path
+    #
+    # config_file_path = Path()
+    # if Path()
+    print(ctx.parent.__dict__)
 
 
 if __name__ == "__main__":
